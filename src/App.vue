1c1903bedac3396ffb67e17b66b92ab9469e429d
workmates@github.io <workmates.live@outlook.com>
2023-05-23 20:33:40 -0500

    s

--- src/App.vue
+++ src/App.vue
@@ -95,7 +95,7 @@ export default {
       "missions": [
         {
           "slug": "001",
-          "name": "Bug-Hunt",
+          "name": "Begin",
           "status": "start"
         },
       ],
