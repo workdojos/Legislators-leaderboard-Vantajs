d2eb0bf1005ae2b485c6a3520ce80aa7fdd20c92
workmates@github.io <workmates.live@outlook.com>
2023-05-23 21:15:59 -0500

    sd

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@
       <div style="height:52px; overflow:hidden;">
         <div class="section-header clipped-medium-backward-pilot">
           <img src="/icons/pilot-icon.svg" />
-          <h1>Pilot Roster</h1>
+          <h1>118th House</h1>
         </div>
         <div class="rhombus-back">&nbsp;</div>
       </div>
